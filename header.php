a410f0cb4a88dd24d366afd357d0a89844231a7a
Phil Kerrigan <phil.kerrigan@gmail.com>
2020-08-12 16:00:10 -0400

    update

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 	<link rel="stylesheet" href="dist/css/all.css" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
@@ -22,11 +23,10 @@
 
     <header role="banner">
 
-
-            <ul class="profile-meta">
-                <li><strong>Howdy, Phil Kerrigan!</strong></li>
-                <li>Logged in as <strong>Admin...</strong></li>
-                <li><a href="#">Account Settings</a></li>
-            </ul>
+        <ul class="profile-meta">
+            <li><strong>Howdy, Phil Kerrigan!</strong></li>
+            <li>Logged in as <strong>Admin...</strong></li>
+            <li><a href="#">Account Settings</a></li>
+        </ul>
 
     </header
--- sidebar.php
+++ sidebar.php
@@ -3,32 +3,14 @@
 
 <img src="/ah-crm/dist/images/ah-logo.png" alt="">
 
-
-<img src="/ah-crm/dist/images/icons/bootstrap.svg" alt="" width="32" height="32" title="">
-
-			<!-- <form method="get" action="/search" id="search">
-				<input name="q" type="text" size="40" placeholder="Search..." />
-			</form> -->
-
 		<nav id="primary">
 			<ul>
-				<li class="accordion-btn"><a href="/ah-crm/order.php"><i class="halflings-icon white list"></i>Orders</a></li>
-				<li class="accordion-btn"><a href="/ah-crm/support.php"><i class="halflings-icon white tag"></i>Support</a></li>
-				<li class="accordion-btn"><a href="/ah-crm/sale.php"><i class="halflings-icon white usd"></i>Sale</a></li>
-				<li class="accordion-btn"><a href="/ah-crm/user.php"><i class="halflings-icon white user"></i>Users</a></li>
-				<li class="accordion-btn"><a href="/ah-crm/return.php"><i class="halflings-icon white repeat"></i>Returns</a></li>
-				<li class="accordion-btn"><a href="#"><span style="font-size:11px; margin-right:4px; position:relative; top:-2px;">&#9660;</span> Reports</a></li>
-				<li>
-					<ul class="accordion-dropdown">
-						<li class="accordion-dropdown-btn"><div class="nav-icon"></div><a href="#"><i class="halflings-icon white list-alt"></i>Rebills $ Retention</a></li>
-						<li class="accordion-dropdown-btn"><a href="#"><i class="halflings-icon white list-alt"></i>Unshipped Rebills</a></li>
-						<li class="accordion-dropdown-btn"><a href="#"><i class="halflings-icon white list-alt"></i>MA & NC Sales</a></li>
-						<li class="accordion-dropdown-btn"><a href="#"><i class="halflings-icon white list-alt"></i>Daily Sales</a></li>
-						<li class="accordion-dropdown-btn"><a href="#"><i class="halflings-icon white list-alt"></i>Summaries</a></li>
-						<li class="accordion-dropdown-btn"><a href="#"><i class="halflings-icon white list-alt"></i>CSR Retention</a></li>
-						<li class="accordion-dropdown-btn"><a href="#"><i class="halflings-icon white list-alt"></i>Age Retention</a></li>
-					</ul>
-				</li>
+				<li><i class="fas fa-user"></i><a href="/ah-crm/order.php">Orders</a></li>
+				<li><a href="/ah-crm/support.php">Support</a></li>
+				<li><a href="/ah-crm/sale.php">Sale</a></li>
+				<li><a href="/ah-crm/user.php">Users</a></li>
+				<li><a href="/ah-crm/return.php">Returns</a></li>
+				<li><a href="#">Reports</a></li>
 			</ul>
 		</nav>
 
